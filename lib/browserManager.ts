0e9da79398316ceb2e48c9f38f82af69e7155318
Jeffery Ozoekwe-Awagu <72825145+Jeffawe@users.noreply.github.com>
2025-08-26 16:54:35 -0400

    Set headless mode depending on where it is deployed at local or web

--- lib/browserManager.ts
+++ lib/browserManager.ts
@@ -4,7 +4,10 @@ let sharedBrowser: Browser | null = null;
 
 export async function getBrowser() {
   if (!sharedBrowser) {
-    sharedBrowser = await chromium.launch({ headless: false });
+    const isProduction = process.env.NODE_ENV === 'production';
+    sharedBrowser = await chromium.launch({
+      headless: isProduction ? true : false
+    });
   }
   return sharedBrowser;
 }
